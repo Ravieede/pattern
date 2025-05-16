196c61bb953c73fe8b3d2f88e042d8e174e54ea7
ravieede <eederavi2@gmail.com>
2025-05-16 14:22:47 +0530

    patterns

---
+++ Pattern_Triangles.java
@@ -0,0 +1,224 @@
+//Patterns_Triangles
+class Pattern_Triangles
+{
+  public static void main(String[] args)
+  {
+    int row=5,col=5;
+    for(int r=0;r<=row;r++)
+     {
+	for(int c=0;c<=r;c++)
+	{
+          System.out.print("# ");
+	}
+         System.out.println();
+     }
+
+     System.out.println("___________");
+      for(int r=0;r<=row;r++)
+     {
+	for(int c=0;c<=r;c++)
+	{
+	  if(c==0||c==r||r==row)
+	  { 
+           System.out.print("# ");
+	  }
+	  else
+          {
+	   System.out.print(" "+" ");	
+	  }
+	}
+	System.out.println();
+     }
+
+     System.out.println("___________");
+      for(int r=0;r<=row;r++)
+     {
+	for(int c=0;c<=r;c++)
+	{
+	  if(r%2!=0)
+	  { 
+           System.out.print("$ ");
+	  }
+	  else
+          {
+	   System.out.print("# ");	
+	  }
+	}
+	System.out.println();
+     }
+    
+    System.out.println("_________");
+     for(int r=0;r<=row;r++)
+     {
+	for(int c=0;c<=r;c++)
+	{
+	  if(c%2!=0)
+	  { 
+           System.out.print("$ ");
+	  }
+	  else
+          {
+	   System.out.print("# ");	
+	  }
+	}
+	System.out.println();
+     }
+    
+     System.out.println("__________");
+       for(int r=1;r<=row;r++)
+     {
+         int temp=1;
+	for(int c=1;c<=r;c++)
+	{
+           System.out.print(temp+" ");
+	   temp++;
+	}
+	System.out.println();
+     }
+
+     System.out.println("____________");
+      for(int r=1;r<=row;r++)
+     {   
+         int temp=1;
+	for(int c=1;c<=r;c++)
+	{
+	   if(r==1)
+	   { 
+               temp++;
+            System.out.print(temp+" ");
+	   }
+           else
+	   {
+ 		 temp++;
+	     System.out.print(temp+" ");
+              
+           }
+
+	}
+	System.out.println();
+     }
+
+
+    System.out.println("_________");
+       for(int r=1;r<=row;r++)
+       {   
+	for(int c=1;c<=r;c++)
+	{
+            System.out.print(r+" ");
+	}
+	System.out.println();
+     }
+
+     System.out.println("_________");
+       for(int r=1;r<=row;r++)
+       {   
+	for(int c=r;c>=1;c--)
+	{
+            System.out.print(c+" ");
+	}
+	System.out.println();
+     }
+     
+     System.out.println("_________");
+       for(int r=1;r<=row;r++)
+       {   
+	  int temp=r;
+	for(int c=r;c>=1;c--)
+	{
+             System.out.print(temp+" ");
+	     temp++;
+	}
+	System.out.println();
+     } 
+
+
+     System.out.println("_________");
+       for(int r=1;r<=row;r++)
+       {   
+	  int temp=row;
+	for(int c=1;c<=r;c++)
+	{
+             System.out.print(temp+" ");
+	     temp--;
+	}
+	System.out.println();
+     }   
+   
+      System.out.println("_________");
+       for(int r=1;r<=row;r++)
+       {   
+        int temp=6-r;
+	for(int c=temp;c<=5;c++)
+	{
+             System.out.print(c+" ");
+	   
+	}
+	System.out.println();
+     } 
+
+    System.out.println("__________");
+       for(int r=1;r<=row;r++)
+       {
+         int temp=1;
+	for(int c=1;c<=r;c++)
+	{
+	   if(c==1||c==r||r==row)
+	   {
+            System.out.print(temp+" ");
+	    temp++;
+	   }
+	   else
+           {
+	   System.out.print(" "+" ");	
+	   }  
+	}
+	System.out.println();
+     }
+   
+       
+    System.out.println("__________");
+       for(int r=1;r<=row;r++)
+       {
+	for(int c=1;c<=r;c++)
+	{
+	   if(c==1||c==r||r==row)
+	   {
+            System.out.print(c+" ");
+	   }
+	   else
+           {
+	   System.out.print(" "+" ");	
+	   }  
+	}
+	System.out.println();
+     }
+
+
+        
+     System.out.println("_________");
+       for(int r=1;r<=row;r++)
+       {   
+	  int temp=r+1;
+	for(int c=1;c<=r;c++)
+	{
+             System.out.print(temp+" ");
+	     temp++;
+	}
+	System.out.println();
+     }   
+
+
+         
+     System.out.println("_________");
+       for(int r=1;r<=row;r++)
+       {  
+	for(int c=1;c<=r;c++)
+	{
+             System.out.print(2*c+" ");
+	}
+	System.out.println();
+     }   
+
+        
+  }
+}
---
+++ pattern_Right_Triangle.java
@@ -0,0 +1,182 @@
+//Right Angle Trinagle
+class pattern_Right_Triangle
+{
+  public static void main(String[] args)
+  {
+     int row=5;
+    for(int r=1;r<=row;r++)
+    {
+       for(int c=row;c>=r;c--)
+       {
+           System.out.print("# ");
+       }
+       System.out.println();
+    }
+     for(int r=1;r<=row;r++)
+    {
+       for(int c=1;c<=r;c++)
+       {
+           System.out.print("# ");
+       }
+       System.out.println();
+    }
+
+    System.out.println("___________");
+    for(int r=1;r<=row;r++)
+    {
+       for(int c=row;c>=r;c--)
+         {
+           if(r==1||r==c||c==row)
+	    { 
+             System.out.print("# ");
+	    }
+           else
+            System.out.print(" "+" ");
+        }
+       System.out.println();
+    }
+     for(int r=1;r<=row;r++)
+    {
+       for(int c=1;c<=r;c++)
+       {
+         if(c==1||c==r||r==row)
+           System.out.print("# ");
+         else
+           System.out.print(" "+" ");
+       }
+       System.out.println();
+    }
+ 
+     System.out.println("_____________");
+      for(int r=1;r<=row;r++)
+    {
+       for(int c=1;c<=r;c++)
+       {
+         if(c==1||r==c)
+           System.out.print("# ");
+         else
+           System.out.print(" ");
+       }
+       System.out.println();
+    }
+     for(int r=2;r<=row;r++)
+    {
+       for(int c=row;c>=r;c--)
+       {
+         if(r==c||c==row)
+           System.out.print("# ");
+         else
+           System.out.print(" ");
+       }
+       System.out.println();
+    }
+ 
+    System.out.println("____________");
+      for(int r=1;r<=row;r++)
+    {
+       for(int c=1;c<=r;c++)
+       {
+           System.out.print("# ");
+       }
+       System.out.println();
+    }
+     for(int r=2;r<=row;r++)
+    {
+       for(int c=row;c>=r;c--)
+       {
+           System.out.print("# ");
+       }
+       System.out.println();
+    }
+   
+   System.out.println("_________________");
+   for(int r=1;r<=row;r++)
+   {
+      for(int c=row;c>=r;c--)
+      {
+         System.out.print("$ ");
+      }
+      for(int c=1;c<=r;c++)
+      {
+        System.out.print("# ");
+      }
+     System.out.println();
+   }
+    
+    System.out.println("_________________");
+   for(int r=1;r<=row;r++)
+   {
+       for(int c=1;c<=r;c++)
+      {
+         System.out.print("# ");
+      }
+      for(int c=row;c>=r;c--)
+      {
+        System.out.print("$ ");
+      }
+     System.out.println();
+   }
+
+
+    System.out.println("_________________");
+     System.out.println("Holo pattern:");
+   for(int r=1;r<=row;r++)
+   {
+      for(int c=row;c>=r;c--)
+      {
+         System.out.print("- ");
+      }
+      for(int c=1;c<=r;c++)
+      {
+         if(r==row||c==r||c==1)
+        {
+          System.out.print("# ");
+        }
+        else
+            System.out.print("  ");
+      }
+     System.out.println();
+   }
+
+    System.out.println("_________________");
+   for(int r=1;r<=row;r++)
+   {
+      for(int c=row;c>r;c--)
+      {
+         System.out.print("  ");
+      }
+      for(int c=1;c<=r;c++)
+      {
+        System.out.print("# ");
+      }
+     System.out.println();
+   }
+
+    System.out.println("_________________");
+    System.out.println("Daimond Shape:");
+   for(int r=1;r<=row;r++)
+   {
+     for(int c=row;c>r;c--)
+      {
+         System.out.print("  ");
+      }
+      for(int c=1;c<r;c++)
+      {
+        System.out.print(" #  ");
+      }
+     System.out.println();
+   }
+    for(int r=2;r<=row;r++)
+    {
+      for(int c=1;c<r;c++)
+      {
+         System.out.print("  ");
+      }
+      for(int c=row;c>r;c--)
+      {
+        System.out.print(" #  ");
+      }
+     System.out.println();
+    }
+  }
+}
